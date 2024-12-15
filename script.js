40a5e15526f5c07c9e3e9134998ab8f9f286e55a
Piptr3 <piptr24@gmail.com>
2024-12-15 23:16:06 +0100

    arrange script file

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+let currentNumber = 0;
+let active = false;
+let secondNumber = 0;
+let currentOperation = "";
+
+const display = document.querySelector('.display');
+const clear = document.querySelector("#clear");
+const zeroButton = document.querySelector("#zero");
+const equals = document.querySelector('#equals');
+const operatorButtons = document.querySelectorAll('.operator');
+const numberButtons = document.querySelectorAll('.number');
+
+clear.addEventListener('click', () => {
+    currentNumber = 0;
+    secondNumber = 0;
+    display.textContent = '0';
+    active = false;
+})
+
+zeroButton.addEventListener('click', () => {
+    if (active) {
+        display.textContent = "";
+        secondNumber = 0;
+        if (display.textContent != '0') {
+            display.textContent = display.textContent + '0';
+        }
+    } else {
+        currentNumber = 0;
+        if (display.textContent != '0') {
+            display.textContent = display.textContent + '0';
+        }
+    }
+})
+
+equals.addEventListener('click', () => {
+    let result = operate(currentNumber, secondNumber, currentOperation)
+    display.textContent = result;
+    currentNumber = result;
+    active = false;
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => handleOperator(button.textContent))
+});
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => handleNumber(Number(button.textContent)));
+});
+
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
@@ -26,20 +75,6 @@ function operate(firstNumber, secondNumber, operator) {
     }
 }
 
-let currentNumber = 0;
-let active = false;
-let secondNumber = 0;
-
-let display = document.querySelector('.display');
-
-const clear = document.querySelector("#clear");
-clear.addEventListener('click', () => {
-    currentNumber = 0;
-    secondNumber = 0;
-    display.textContent = '0';
-    active = false;
-})
-
 function handleNumber(number) {
     if (active) {
         display.textContent = "";
@@ -59,40 +94,3 @@ function handleOperator(operator) {
     currentOperation = operator;
     active = true;
 }
-
-const numberButtons = document.querySelectorAll('.number');
-numberButtons.forEach((button) => {
-    button.addEventListener('click', () => handleNumber(Number(button.textContent)));
-});
-
-const operatorButtons = document.querySelectorAll('.operator');
-operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => handleOperator(button.textContent))
-});
-
-const zeroButton = document.querySelector("#zero");
-zeroButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 0;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '0';
-        }
-    } else {
-        currentNumber = 0;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '0';
-        }
-    }
-})
-
-let currentOperation = "";
-
-const equals = document.querySelector('#equals');
-
-equals.addEventListener('click', () => {
-    let result = operate(currentNumber, secondNumber, currentOperation)
-    display.textContent = result;
-    currentNumber = result;
-    active = false;
-});
