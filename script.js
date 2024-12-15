8dcab614fef7aa3db6ac737c14630daf3c4f232c
Piptr3 <piptr24@gmail.com>
2024-12-15 23:12:04 +0100

    remove repetiton with operators

--- script.js
+++ script.js
@@ -40,6 +40,36 @@ clear.addEventListener('click', () => {
     active = false;
 })
 
+function handleNumber(number) {
+    if (active) {
+        display.textContent = "";
+        secondNumber = number;
+        display.textContent = display.textContent + number;      
+    } else {
+        currentNumber = number;
+        if (display.textContent != '0') {
+            display.textContent = display.textContent + number;
+        } else {
+            display.textContent = number;
+        }
+    }
+}
+
+function handleOperator(operator) {
+    currentOperation = operator;
+    active = true;
+}
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => handleNumber(Number(button.textContent)));
+});
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => handleOperator(button.textContent))
+});
+
 const zeroButton = document.querySelector("#zero");
 zeroButton.addEventListener('click', () => {
     if (active) {
@@ -54,178 +84,9 @@ zeroButton.addEventListener('click', () => {
             display.textContent = display.textContent + '0';
         }
     }
-   
-})
-
-const oneButton = document.querySelector("#one");
-oneButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 1;
-        display.textContent = display.textContent + '1';      
-    } else {
-        currentNumber = 1;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '1';
-        } else {
-            display.textContent = '1';
-        }
-    }
-})
-
-const twoButton = document.querySelector("#two");
-twoButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 2;
-        display.textContent = display.textContent + '2';      
-    } else {
-        currentNumber = 2;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '2';
-        } else {
-            display.textContent = '2';
-        }
-    }
-})
-
-const threeButton = document.querySelector("#three");
-threeButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 3;
-        display.textContent = display.textContent + '3';      
-    } else {
-        currentNumber = 3;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '3';
-        } else {
-            display.textContent = '3';
-        }
-    }
-})
-
-const fourButton = document.querySelector("#four");
-fourButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 4;
-        display.textContent = display.textContent + '4';      
-    } else {
-        currentNumber = 4;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '4';
-        } else {
-            display.textContent = '4';
-        }
-    }
-})
-
-const fiveButton = document.querySelector("#five");
-fiveButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 5;
-        display.textContent = display.textContent + '5';      
-    } else {
-        currentNumber = 5;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '5';
-        } else {
-            display.textContent = '5';
-        }
-    }
-})
-
-const sixButton = document.querySelector("#six");
-sixButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 6;
-        display.textContent = display.textContent + '6';      
-    } else {
-        currentNumber = 6;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '6';
-        } else {
-            display.textContent = '6';
-        }
-    }
-})
-
-const sevenButton = document.querySelector("#seven");
-sevenButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 7;
-        display.textContent = display.textContent + '7';      
-    } else {
-        currentNumber = 7;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '7';
-        } else {
-            display.textContent = '7';
-        }
-    }
-})
-
-const eightButton = document.querySelector("#eight");
-eightButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 8;
-        display.textContent = display.textContent + '8';      
-    } else {
-        currentNumber = 5;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '8';
-        } else {
-            display.textContent = '8';
-        }
-    }
-})
-
-const nineButton = document.querySelector("#nine");
-nineButton.addEventListener('click', () => {
-    if (active) {
-        display.textContent = "";
-        secondNumber = 9;
-        display.textContent = display.textContent + '9';      
-    } else {
-        currentNumber = 9;
-        if (display.textContent != '0') {
-            display.textContent = display.textContent + '9';
-        } else {
-            display.textContent = '9';
-        }
-    }
 })
 
 let currentOperation = "";
-
-const addButton = document.querySelector('#add');
-addButton.addEventListener('click', () => {
-    currentOperation = '+';
-    active = true;
-})
-
-const subtractButton = document.querySelector('#subtract');
-subtractButton.addEventListener('click', () => {
-    currentOperation = '-';
-    active = true;
-})
-
-const multiplyButton = document.querySelector('#multiply');
-multiplyButton.addEventListener('click', () => {
-    currentOperation = '*';
-    active = true;
-})
-
-const divideButton = document.querySelector('#divide');
-divideButton.addEventListener('click', () => {
-    currentOperation = '/';
-    active = true;
-})
 
 const equals = document.querySelector('#equals');
 
